93764c9b01ec000dadcc585b69005e3617d2a22e
Joe Sweeny <joesweeny84@hotmail.com>
2017-06-20 21:22:51 +0100

    Update user controller and tests

--- src/app/Application/Http/Api/v1/Routing/User/RouteManager.php
+++ src/app/Application/Http/Api/v1/Routing/User/RouteManager.php
@@ -3,6 +3,7 @@
 namespace Cocktales\Application\Http\Api\v1\Routing\User;
 
 use Cocktales\Application\Http\Api\v1\Controllers\User\Register;
+use Cocktales\Application\Http\Api\v1\Controllers\User\Update;
 use Cocktales\Application\Http\RouteMapper;
 use FastRoute\RouteCollector;
 
@@ -16,5 +17,6 @@ class RouteManager implements RouteMapper
     public function map(RouteCollector $router)
     {
         $router->addRoute('POST', '/api/v1/user/register', Register::class);
+        $router->addRoute('POST', '/api/v1/user/update', Update::class);
     }
 }

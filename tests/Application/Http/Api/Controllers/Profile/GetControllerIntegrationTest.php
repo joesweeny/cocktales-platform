bea0c9cac5d3edff424b2edf1a87b197d96e4643
Joe Sweeny <joesweeny84@hotmail.com>
2017-07-28 20:35:24 +0100

    Additional test added

--- tests/Application/Http/Api/Controllers/Profile/GetControllerIntegrationTest.php
+++ tests/Application/Http/Api/Controllers/Profile/GetControllerIntegrationTest.php
@@ -40,7 +40,7 @@ class GetControllerIntegrationTest extends TestCase
         $response = $this->handle($this->container, $request);
 
         $jsend = json_decode($response->getBody()->getContents());
-        
+
         $this->assertEquals('success', $jsend->status);
         $this->assertEquals('b5acd30c-085e-4dee-b8a9-19e725dc62c3', $jsend->data->profile->user_id);
         $this->assertEquals('joe', $jsend->data->profile->username);
@@ -48,6 +48,20 @@ class GetControllerIntegrationTest extends TestCase
         $this->assertEquals('Sweeny', $jsend->data->profile->last_name);
         $this->assertEquals('Essex', $jsend->data->profile->location);
         $this->assertEquals('Be drunk and Merry', $jsend->data->profile->slogan);
+    }
+
+    public function test_fail_response_is_received_if_profile_does_not_exist()
+    {
+        $request = new ServerRequest('GET', '/api/v1/profile/get', [],
+            '{"user_id" : "b5acd30c-085e-4dee-b8a9-19e725dc62c3"}'
+        );
+
+        $response = $this->handle($this->container, $request);
+
+        $jsend = json_decode($response->getBody()->getContents());
+
+        $this->assertEquals('fail', $jsend->status);
+        $this->assertEquals('Unable to retrieve profile', $jsend->data->error);
     }
 
     private function createProfile()

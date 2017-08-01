12f33338a6d2ecc2ca7bb899e2f8947e1b33ba66
Joe Sweeny <joesweeny84@hotmail.com>
2017-08-01 13:13:37 +0100

    CreateAvatarCommand and Handler

--- src/app/Boundary/Avatar/Command/CreateAvatarCommand.php
+++ src/app/Boundary/Avatar/Command/CreateAvatarCommand.php
@@ -3,8 +3,57 @@
 namespace Cocktales\Boundary\Avatar\Command;
 
 use Cocktales\Framework\CommandBus\Command;
+use Cocktales\Framework\Uuid\Uuid;
 
 class CreateAvatarCommand implements Command
 {
+    /**
+     * @var string
+     */
+    private $thumbnail;
+    /**
+     * @var string
+     */
+    private $standardSize;
+    /**
+     * @var string
+     */
+    private $user_id;
 
+    /**
+     * CreateAvatarCommand constructor.
+     * @param string $user_id
+     * @param string $thumbnail
+     * @param string $standardSize
+     */
+    public function __construct(string $user_id, string $thumbnail, string $standardSize)
+    {
+        $this->user_id = $user_id;
+        $this->thumbnail = $thumbnail;
+        $this->standardSize = $standardSize;
+    }
+
+    /**
+     * @return Uuid
+     */
+    public function getUserId(): Uuid
+    {
+        return new Uuid($this->user_id);
+    }
+
+    /**
+     * @return string
+     */
+    public function getThumbnail(): string
+    {
+        return $this->thumbnail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStandardSize(): string
+    {
+        return $this->standardSize;
+    }
 }
---
+++ src/app/Boundary/Avatar/Command/Handlers/CreateAvatarCommandHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cocktales\Domain\Avatar\Command\Handlers;
+
+use Cocktales\Boundary\Avatar\Command\CreateAvatarCommand;
+use Cocktales\Domain\Avatar\AvatarOrchestrator;
+use Cocktales\Domain\Avatar\Entity\Avatar;
+
+class CreateAvatarCommandHandler
+{
+    /**
+     * @var AvatarOrchestrator
+     */
+    private $orchestrator;
+
+    /**
+     * CreateAvatarCommandHandler constructor.
+     * @param AvatarOrchestrator $orchestrator
+     */
+    public function __construct(AvatarOrchestrator $orchestrator)
+    {
+        $this->orchestrator = $orchestrator;
+    }
+
+    /**
+     * @param CreateAvatarCommand $command
+     * @return Avatar
+     * @throws \Cocktales\Domain\Avatar\Exception\AvatarRepositoryException
+     */
+    public function handle(CreateAvatarCommand $command): Avatar
+    {
+        return $this->orchestrator->createAvatar((new Avatar)
+            ->setUserId($command->getUserId())
+            ->setThumbnail($command->getThumbnail())
+            ->setStandard($command->getStandardSize()));
+    }
+}

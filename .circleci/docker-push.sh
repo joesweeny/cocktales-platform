c23dff2e2642348eafdd58c1748fc913e11668b4
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-03 14:15:37 +0000

    Update docker login script

--- .circleci/docker-push.sh
+++ .circleci/docker-push.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-docker login --username=${DOCKER_HUB_USERNAME} --email={DOCKER_HUB_EMAIL}
-${DOCKER_HUB_PASSWORD}
+docker login -u ${DOCKER_HUB_USERNAME} -p {DOCKER_HUB_PASSWORD}
 docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
 docker push "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"

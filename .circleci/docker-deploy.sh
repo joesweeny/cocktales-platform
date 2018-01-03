1fbb16543170bcb848e081f908b85b6b19cfee12
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-03 20:10:01 +0000

    Update execution path

--- .circleci/docker-deploy.sh
+++ .circleci/docker-deploy.sh
@@ -3,4 +3,4 @@
 set -e
 
 scp docker-compose.staging.yml root@${STAGING_SERVER}:
-ssh root@${STAGING_SERVER} "docker-compose down -v; docker-compose -f docker-compose.staging.yml up -d --build"
+ssh root@${STAGING_SERVER} "docker-compose -f docker-compose.staging.yml down -v; docker-compose -f docker-compose.staging.yml up -d --build"

b746feb30d4c3d88debfec5f52e090e3fdd24697
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-03 14:30:33 +0000

    Add email to Docker login

--- .circleci/docker-push.sh
+++ .circleci/docker-push.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker login -u ${DOCKER_HUB_USERNAME} --password-stdin {DOCKER_HUB_PASSWORD}
+docker login -u ${DOCKER_HUB_USERNAME} --password-stdin {DOCKER_HUB_PASSWORD} -e {DOCKER_HUB_EMAIL}
 docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
 docker push "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"

672b5c6a286a54a7fbaa80512ba24e2f5597e416
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-03 14:54:06 +0000

    Improve docker login

--- .circleci/docker-push.sh
+++ .circleci/docker-push.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-${DOCKER_HUB_PASSWORD} | docker login -u ${DOCKER_HUB_USERNAME} --password-stdin
+docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 docker tag "${DOCKER_IMAGE}:latest" "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"
 docker push "${DOCKER_HUB_REPOSITORY}/${DOCKER_IMAGE}:latest"

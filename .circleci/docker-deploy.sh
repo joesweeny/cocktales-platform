46566e59058e180d01973ebd7ae6ff2e51248a89
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-03 19:42:11 +0000

    Update docker-compose file to see if it updates

--- .circleci/docker-deploy.sh
+++ .circleci/docker-deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-scp docker-compose.staging.yml root@${STAGING_SERVER}:~/
+scp docker-compose.staging.yml root@${STAGING_SERVER}:
 ssh root@${STAGING_SERVER} "docker-compose -f docker-compose.staging.yml up -d"

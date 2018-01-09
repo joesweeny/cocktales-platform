79a06918763419809bc4d078d8897aee408b2482
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-09 13:06:18 +0000

    Update commands and handlers and remove Avatar presenter

--- src/app/Boundary/Avatar/Command/Handlers/UpdateAvatarCommandHandler.php
+++ src/app/Boundary/Avatar/Command/Handlers/UpdateAvatarCommandHandler.php
@@ -2,10 +2,8 @@
 
 namespace Cocktales\Boundary\Avatar\Command\Handlers;
 
-use Cocktales\Boundary\Avatar\AvatarPresenter;
 use Cocktales\Boundary\Avatar\Command\UpdateAvatarCommand;
 use Cocktales\Domain\Avatar\AvatarOrchestrator;
-use Cocktales\Domain\Avatar\Entity\Avatar;
 
 class UpdateAvatarCommandHandler
 {
@@ -13,40 +11,22 @@ class UpdateAvatarCommandHandler
      * @var AvatarOrchestrator
      */
     private $orchestrator;
-    /**
-     * @var AvatarPresenter
-     */
-    private $presenter;
 
     /**
      * UpdateAvatarCommandHandler constructor.
      * @param AvatarOrchestrator $orchestrator
-     * @param AvatarPresenter $presenter
      */
-    public function __construct(AvatarOrchestrator $orchestrator, AvatarPresenter $presenter)
+    public function __construct(AvatarOrchestrator $orchestrator)
     {
         $this->orchestrator = $orchestrator;
-        $this->presenter = $presenter;
     }
 
     /**
      * @param UpdateAvatarCommand $command
-     * @return \stdClass
-     * @throws \Cocktales\Framework\Exception\NotFoundException
-     * @throws \League\Flysystem\FileExistsException
+     * @return void
      */
-    public function handle(UpdateAvatarCommand $command): \stdClass
+    public function handle(UpdateAvatarCommand $command): void
     {
-        $avatar = $this->orchestrator->getAvatarByUserId($command->getUserId());
-
-        $filename = $command->getUserId()->__toString() . '.' . $command->getFile()->getClientOriginalExtension();
-
-        $this->orchestrator->saveThumbnailToStorage($command->getFile(), "/avatar/{$command->getUserId()}/thumbnail/" . $filename);
-
-        $this->orchestrator->saveStandardSizeToStorage($command->getFile(), "/avatar/{$command->getUserId()}/standard/" . $filename);
-
-        return $this->presenter->toDto($this->orchestrator->updateAvatar($avatar->getUserId(), function (Avatar $avatar) use ($filename) {
-            $avatar->setFilename($filename);
-        }));
+        $this->orchestrator->updateAvatar($command->getUserId(), $command->getFileContents());
     }
 }
--- src/app/Boundary/Avatar/Command/UpdateAvatarCommand.php
+++ src/app/Boundary/Avatar/Command/UpdateAvatarCommand.php
@@ -4,7 +4,6 @@ namespace Cocktales\Boundary\Avatar\Command;
 
 use Cocktales\Framework\CommandBus\Command;
 use Cocktales\Framework\Uuid\Uuid;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UpdateAvatarCommand implements Command
 {
@@ -13,28 +12,28 @@ class UpdateAvatarCommand implements Command
      */
     private $userId;
     /**
-     * @var UploadedFile
+     * @var string
      */
-    private $file;
+    private $fileContents;
 
     /**
      * UpdateAvatarCommand constructor.
      * @param string $userId
-     * @param UploadedFile $file
+     * @param string $fileContents
      */
-    public function __construct(string $userId, UploadedFile $file)
+    public function __construct(string $userId, string $fileContents)
     {
-        $this->userId = new Uuid($userId);
-        $this->file = $file;
+        $this->userId = $userId;
+        $this->fileContents = $fileContents;
     }
 
     public function getUserId(): Uuid
     {
-        return $this->userId;
+        return new Uuid($this->userId);
     }
 
-    public function getFile(): UploadedFile
+    public function getFileContents(): string
     {
-        return $this->file;
+        return $this->fileContents;
     }
 }

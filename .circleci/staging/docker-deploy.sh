e95e74a773fec52ed8b7e2aa829392ad24d9d674
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-01-24 17:30:41 +0000

    Update deploy script

--- .circleci/staging/docker-deploy.sh
+++ .circleci/staging/docker-deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 scp docker-compose.staging.yml root@${STAGING_SERVER}:~/staging
 scp ./circleci/staging/env.sh root@{$STAGING_SERVER}:
-ssh root@${STAGING_SERVER} "source env.sh && docker-compose -f ./staging/docker-compose.staging.yml pull web && docker-compose -f ./staging/docker-compose.staging.yml up -d"
+ssh root@${STAGING_SERVER} "docker-compose -f ./staging/docker-compose.staging.yml pull web && docker-compose -f ./staging/docker-compose.staging.yml up -d"
